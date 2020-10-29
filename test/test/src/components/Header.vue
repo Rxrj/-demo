5e7c2f6c1dbcd7d43853eec5fd950f0727fad418
Rxrj <994010524@qq.com>
2020-10-29 17:51:18 +0800

    add the choose of partition and charts

--- test/test/src/components/Header.vue
+++ test/test/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <el-header>
-    <el-menu :default-active="this.$route.path" router class="el-menu-demo" mode="horizontal"  @select="handleSelect" style="background-color: #252525;border-bottom: solid 2px #444444" active-text-color="#409EFF">
+    <el-menu :default-active="this.$route.path" router class="el-menu-demo" mode="horizontal" style="background-color: #252525;border-bottom: solid 2px #444444" active-text-color="#409EFF">
       <el-menu-item class="header-item" style="font-size: 27px;width: 140px">S  O  U  P</el-menu-item>
       <el-menu-item disabled class="icon" style="font-size: 10px;color:#eeeeee;width:180px;line-height: 11px;padding-top: 20px;padding-left: 0">Spatial-Temporal Demand<br/> Forecasting and Competitive Supply</el-menu-item>
       <el-menu-item index="/" class="header-item">Supply-Demand Analysis</el-menu-item>
--- test/test/src/components/Slider.vue
+++ test/test/src/components/Slider.vue
@@ -10,7 +10,7 @@
           :picker-options="pickerOptions">
         </el-date-picker>
         <br>
-        <el-checkbox v-model="checked" style="padding-top: 20px">Partition Visible</el-checkbox>
+        <el-checkbox v-model="checked" style="padding-top: 20px" @change="handleChange">Partition Visible</el-checkbox>
         <br>
         <el-button style="margin-top: 20px;width: 220px;background-color:#2d2d2d;border:solid 2px #444444;color: #eeeeee">Start</el-button>
       </div>
@@ -65,6 +65,11 @@ export default {
       }],
       value: ''
     };
+  },
+  methods:{
+    handleChange(){
+      this.emit('handleChange',this.checked);
+    }
   }
 }
 </script>
